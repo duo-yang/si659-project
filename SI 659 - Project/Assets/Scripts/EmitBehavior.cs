c1875777b20f8ff87fc9714f99a506785aa693e7
Duo YANG <yangduots@163.com>
2019-04-12 04:24:59 -0400

    Add toggles

---
+++ SI 659 - Project/Assets/Scripts/EmitBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitBehavior : MonoBehaviour {
+
+  public float intensityMin = 0.5F;
+  public float intensityMax = 2.5F;
+  public float lightMin = 0F;
+  public float lightMax = 0.8F;
+  public float defaultIntensity = 1F;
+
+  public TextMesh intensityText;
+
+  private float _defaultLight = 0F;
+  private Color _buttonColor = Color.black;
+  private Renderer _rend;
+
+  // Use this for initialization
+  void Start () {
+    _rend = GetComponent<Renderer>();
+    _defaultLight = mapIntensity(defaultIntensity);
+    setButtonColor(_defaultLight);
+    if (intensityText != null) intensityText.text = defaultIntensity.ToString();
+  }
+	
+	float mapIntensity(float intensity) {
+    if (intensity < intensityMin || intensity > intensityMax) return _defaultLight;
+    return (intensity - intensityMin) / (intensityMax - intensityMin) * (lightMax - lightMin) + lightMin;
+  }
+
+  void setButtonColor(float light) {
+    _buttonColor.r = light;
+    _buttonColor.g = light;
+    _buttonColor.b = light;
+  }
+
+  public void updateButtonLight(float intensity) {
+    setButtonColor(mapIntensity(intensity));
+    _rend.material.SetColor("_EmissionColor", _buttonColor);
+    if (intensityText != null) intensityText.text = intensity.ToString("F");
+  }
+}
---
+++ SI 659 - Project/Assets/Scripts/ModeBehavior.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeBehavior : MonoBehaviour {
+
+  public MonoBehaviour[] modeControllers;
+  public Color[] modeColors;
+  public string[] modeNamesText;
+
+  public TextMesh buttonText;
+  
+  private Renderer _rend;
+  private int _mode = 0;
+  private int _numMode = 0;
+
+  void OnValidate() {
+    if (modeControllers.Length != modeNamesText.Length) {
+      Debug.LogWarning("Number of mode controllers does not match number of their names.");
+    }
+  }
+
+  // Use this for initialization
+  void Start () {
+    _rend = GetComponent<Renderer>();
+    _rend.material.SetColor("_EmissionColor", Color.black);
+    if (modeControllers.Length == modeNamesText.Length) {
+      _numMode = modeControllers.Length;
+    }
+    useMode(_mode);
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  public void useMode(int mode) {
+    // Enable corresponding mode and disable others
+    for (int i = 0; i < _numMode; i++) {
+      modeControllers[i].enabled = (i == mode) ? true : false;
+    }
+    // Set button color
+    _rend.material.SetColor("_EmissionColor", (modeColors.Length > mode) ? modeColors[mode] : Color.black);
+    // Set button text
+    if (buttonText != null) buttonText.text = modeNamesText[mode];
+
+  }
+
+  public void toggleMode () {
+    if (_mode < _numMode - 1) {
+      _mode++;
+    } else {
+      _mode = 0;
+    }
+    useMode(_mode);
+  }
+
+}
