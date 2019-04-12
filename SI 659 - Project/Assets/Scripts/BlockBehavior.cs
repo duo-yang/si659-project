dc114901c1a5b204d3b32ab5512ff1aac7393957
Duo YANG <yangduots@163.com>
2019-04-12 07:02:27 -0400

    Add color picker

---
+++ SI 659 - Project/Assets/Scripts/BlockBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehavior : MonoBehaviour {
+
+  public ColorBehavior colorManager;
+
+  private GameObject _innerCube;
+  private Light _pointLight;
+  private Renderer _rend;
+
+  private bool _notNull = false;
+
+  void Awake() {
+    colorManager = ColorBehavior.colorManagerInstance;
+  }
+
+  // Use this for initialization
+  void Start () {
+    _innerCube = this.transform.Find("Cube").gameObject;
+    _pointLight = this.transform.Find("Point Light").gameObject.GetComponent<Light>();
+    if (_innerCube != null && _pointLight != null) {
+      _rend = _innerCube.GetComponent<Renderer>();
+      _notNull = true;
+      setColor(colorManager.getColor());
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  public void setColor(Color color) {
+    if (_notNull) {
+      _rend.material.SetColor("_Color", color);
+      _rend.material.SetColor("_EmissionColor", color);
+      _pointLight.color = color;
+    }
+  }
+}
--- SI 659 - Project/Assets/Scripts/ColorBehavior.cs
+++ SI 659 - Project/Assets/Scripts/ColorBehavior.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class ColorBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+  public static ColorBehavior colorManagerInstance;
+
+  public BlockBehavior colorPicker;
+
+  public Color[] palette;
+  public int currentColorID = 0;
+
+  private int _numColor = 0;
+
+  void Awake() {
+    colorManagerInstance = this;
+  }
+
+  // Use this for initialization
+  void Start () {
+    if (palette != null) _numColor = palette.Length;
+    if (colorPicker != null) colorPicker.setColor(getColor());
+    currentColorID = 0;
+  }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+  public Color getColor(int colorID) {
+    return (colorID < _numColor) ? palette[colorID] : Color.white;
+  }
+  public Color getColor() {
+    return getColor(currentColorID);
+  }
+
+  public void updateColorID(int colorID) {
+    if (colorID >= 0 && colorID < _numColor) currentColorID = colorID;
+    if (colorPicker != null) colorPicker.setColor(getColor());
+  }
 }
